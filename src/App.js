2633621e35f608efc86332416b87e108d50fa349
Umer Mughal <itsmeumer966@gmail.com>
2021-09-27 18:24:34 +0500

    Fix Cookies issue

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import { BrowserRouter } from "react-router-dom";
 import Routes from "./routes/Routes";
 // import Footer from './components/Footer';
 // import Landing from '../src/screens/Landing';
+import axios from 'axios'
 
 function App() {
+
+    axios.defaults.withCredentials = true
+
+
+
   return (
     <div className="App">
       <BrowserRouter>
